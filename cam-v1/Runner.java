a9f18f87d36a39986520ae78fcec22b1e2f57996
Hugo255n <Hugo255n@ahs.local>
2023-10-11 15:05:29 -0700

    day1

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,40 @@
+import java.awt.Component;
+
+import javax.swing.*;
+
 public class Runner {
-    public static void main(String[] args) {
+    public Runner() {
         VirtualPet p1 = new VirtualPet();
 
-        p1.feed();
+        Object[] personalities = {"Enthusiastic", "Pessimistic", "Mundane", "Ravenous", "Daring"};
+
+        p1.exercise();
+        wait(1000);
+        String ans = getResponse("How was your day?");
+        System.out.println(ans);
+
+        JOptionPane.showMessageDialog(null, "Press OK");
+    }
+
+    public static void main(String[] args) {
+        new Runner();
+    }
+
+    public void wait(int milliseconds) {
+        try {
+            Thread.sleep(milliseconds);
+        } catch (Exception e) {
+
+        }
+    }
+
+    public String getResponse(String q) {
+        String s = (String) JOptionPane.showInputDialog(
+                new JFrame(),
+                q,
+                "Your Title",
+                JOptionPane.PLAIN_MESSAGE
+        );
+        return s;
     }
 }
